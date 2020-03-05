129c2571ef3d6f3c2025e66dfa9fb08d22fb4efd
fatihsevban <fatihsevban15@gmail.com>
2020-03-05 21:05:44 +0300

    initial commit

---
+++ src/ArrayOrganizer.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class ArrayOrganizer {
+
+    public ArrayList<Integer> concatAndSort(ArrayList<Integer> list1, ArrayList<Integer> list2) {
+        ArrayList<Integer> result = new ArrayList<>();
+        result.addAll(list1);
+        result.addAll(list2);
+        Collections.sort(result);
+        return result;
+    }
+
+}

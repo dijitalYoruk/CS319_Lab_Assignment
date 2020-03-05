6209ff165863f2ceaf21bc85b5d92ba1eeb5e904
fatihsevban <fatihsevban15@gmail.com>
2020-03-05 22:16:57 +0300

    Three lists can be concatenated in ArraySorter.

--- src/ArrayOrganizer.java
+++ src/ArrayOrganizer.java
@@ -3,10 +3,11 @@ import java.util.Collections;
 
 public class ArrayOrganizer {
 
-    public ArrayList<Integer> concatAndSort(ArrayList<Integer> list1, ArrayList<Integer> list2) {
+    public ArrayList<Integer> concatAndSort(ArrayList<Integer> list1, ArrayList<Integer> list2, ArrayList<Integer> list3) {
         ArrayList<Integer> result = new ArrayList<>();
         result.addAll(list1);
         result.addAll(list2);
+        result.addAll(list3);
         Collections.sort(result);
         return result;
     }
